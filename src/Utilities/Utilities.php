4cebddea61d26b5aea387cd04f61d43ac0e50e3c
Mohsen Safari <safari.tafreshi@gmail.com>
2021-08-16 20:46:25 +0430

    returnName added

--- src/Utilities/Utilities.php
+++ src/Utilities/Utilities.php
@@ -5,4 +5,9 @@ class Utilities {
     public static function packageInfo() {
         return "0.0.1";
     }
+
+
+    public static function returnName() {
+        return "Mohsen safari";
+    }
 }
